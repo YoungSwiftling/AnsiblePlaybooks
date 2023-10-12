0aaf15c4b6ac6eb980f8cba157e83fddf917d6dc
Alex Ngounou <alexngounou@gmail.com>
2023-10-12 12:49:10 -0400

    Added the linux.sh file

---
+++ linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter your name:"
+read name
+echo "Reading..... please wait"
+sleep 3
+echo "-------------------------------"
+echo "Your username is:" && whoami
+
+#This is a test file to be pushed to github.
